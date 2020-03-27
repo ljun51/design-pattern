2d4208b15a6b4930dc1681af7800aac7b8882c75
ljun51 <ljun51@outlook.com>
2020-03-27 08:14:48 +0800

    chain of responsibility pattern

---
+++ src/headfirst/designpatterns/responsibilitychain/Handler.java
@@ -0,0 +1,23 @@
+package headfirst.designpatterns.responsibilitychain;
+
+public abstract class Handler {
+
+    protected Handler successor;
+
+    /**
+     * 示意处理请求的方法，虽然这个示意方法是没有传入参数的
+     * 但实际是可以传入参数的，根据具体需要来选择是否传递参数
+     */
+    public abstract void handleRequest();
+
+    public Handler getSuccessor() {
+        return successor;
+    }
+
+    /**
+     * 赋值方法，设置后继的责任对象
+     */
+    public void setSuccessor(Handler successor) {
+        this.successor = successor;
+    }
+} 
